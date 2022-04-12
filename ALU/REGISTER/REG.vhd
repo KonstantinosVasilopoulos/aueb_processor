d728862f5ddf491c20d81fc363fd8a12c50d7bcc
KonstantinosVasilopoulos <costasvas34@protonmail.com>
2022-04-12 19:38:30 +0300

    ALU connected with registers

--- ALU/REGISTER/REG.vhd
+++ ALU/REGISTER/REG.vhd
@@ -1,13 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity FLIP_FLOP_16bit is
+entity reg is
 port(d: in std_logic_vector(15 downto 0);
 	clock, enable: in std_logic;
 	out1: out std_logic_vector(15 downto 0));
-end FLIP_FLOP_16bit;
+end reg;
 
-architecture FlipFlop16BitLogic of FLIP_FLOP_16bit is
+architecture RegLogic of reg is
 	component FLIP_FLOP_1bit is
 	port(d, clock, enable: in std_logic;
 		out1, out2: out std_logic);
@@ -19,6 +19,6 @@ architecture FlipFlop16BitLogic of FLIP_FLOP_16bit is
 begin	
 	-- Create 16 flip-flops
 	flip_flops_loop: for i in 15 downto 0 generate
-		FF160: FLIP_FLOP_1bit port map(d(i), clock, enable, out1(i), flip_flop_not(i));
+		REG0: FLIP_FLOP_1bit port map(d(i), clock, enable, out1(i), flip_flop_not(i));
 	end generate flip_flops_loop;
-end FlipFlop16BitLogic;
+end RegLogic;
--- AUEB_PROCESSOR.vhd
+++ AUEB_PROCESSOR.vhd
@@ -1,12 +1,48 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- Κωνσταντινος Βασιλοπουλος 3180018
+-- Ευθυμιος Παπαγεωργιου 3180144
+
+-- Παρατηρηση
+-- Παρατηρουμε πως οι registers καθυστερουν ενα κυκλο ρολογιου καθε φορα πρωτου αποκτησουν
+-- τιμες. Πιο συγκεκριμενα, οι δυο πρωτοι καταχωρητες αποκτουν τιμες στο δευτερο clock pulse,
+-- ενω ο τριτος καταχωρητης στο τριτο pulse. Αυτο σημαινει, πως και η ALU μπορει να ξεκινησει 
+-- τους υπολογισμους της κατα τον δευτερο παλμο.
+
 entity AUEB_PROCESSOR is
-port(in1: in std_logic;
-	out1: out std_logic);
+port(in1, in2: in std_logic_vector(15 downto 0);
+	clock: in std_logic;
+	operation: in std_logic_vector(2 downto 0);
+	out1, out2, aluout, output: out std_logic_vector(15 downto 0));
 end AUEB_PROCESSOR;
 
 architecture ProcessorLogic of AUEB_PROCESSOR is
+	component reg is
+	port(d: in std_logic_vector(15 downto 0);
+		clock, enable: in std_logic;
+		out1: out std_logic_vector(15 downto 0));
+	end component reg;
+
+	component ALU_16bit is
+	port(in1, in2: in std_logic_vector(15 downto 0);
+		operation: in std_logic_vector(2 downto 0);
+		out1: out std_logic_vector(15 downto 0));
+	end component ALU_16bit;
+
+	signal first_reg, second_reg, alu_result: std_logic_vector(15 downto 0);
+
 begin
-	out1 <= in1;
+	-- First and second registers
+	P0: reg port map(in1, clock, '1', first_reg);
+	P1: reg port map(in2, clock, '1', second_reg);
+	out1 <= first_reg;
+	out2 <= second_reg;
+
+	-- ALU
+	P2: ALU_16bit port map(first_reg, second_reg, operation, alu_result);
+	aluout <= alu_result;
+
+	-- Final register
+	P3: reg port map(alu_result, clock, '1', output);
 end ProcessorLogic;
